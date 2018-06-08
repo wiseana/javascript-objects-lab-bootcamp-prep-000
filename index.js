06fe7308e95f4dc7a0dc6a222f2ec6b0a41bf177
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-08 01:45:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ function updateObjectWithKeyAndValue(object, key, value) {
   }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
-  return object;}
+  return object;
+  }
+function deleteFromObjectByKey(object,key) {
+  return Object.assign(object, )
+}
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
