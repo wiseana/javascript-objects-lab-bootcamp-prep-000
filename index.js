8f3230008ec80e9fca1d321b638331b162af1751
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-08 01:34:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {ingredients : "ammount"};
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+  }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;}
+function deleteFromObjectByKey(object, key) {
+  
+}  
